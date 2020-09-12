d95d1d7888db63f78c7a1197a082629bb343d30e
Shivanshmathur3 <shivansh_mathur@hotmail.com>
2020-09-12 14:15:56 +0530

    heroku database

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-$connection=mysqli_connect('localhost','root','','verdeguatemala');
+$connection=mysqli_connect('us-cdbr-east-02.cleardb.com','b1166797b033d6','7e351580','heroku_7682f9ee17b688a');
 if(!$connection){
     die('QUERRY FAILED'.mysqli_error($connection));
 }
